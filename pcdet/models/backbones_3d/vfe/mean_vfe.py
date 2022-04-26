b9eca0fbf98e80246daea17e77c3251f785eb23f
xiangruhuang <xiangruhuang816@gmail.com>
2022-04-26 15:21:23 -0400

    fix seg cls bug

--- pcdet/models/backbones_3d/vfe/mean_vfe.py
+++ pcdet/models/backbones_3d/vfe/mean_vfe.py
@@ -22,11 +22,19 @@ class MeanVFE(VFETemplate):
         Returns:
             vfe_features: (num_voxels, C)
         """
-        voxel_features, voxel_num_points = batch_dict['voxels'], batch_dict['voxel_num_points']
+        voxel_features, voxel_num_points = batch_dict['voxels'], batch_dict['voxel_num_points'].long()
         points_mean = voxel_features[:, :, :].sum(dim=1, keepdim=False)
         normalizer = torch.clamp_min(voxel_num_points.view(-1, 1), min=1.0).type_as(voxel_features)
         points_mean = points_mean / normalizer
         batch_dict['voxel_features'] = points_mean.contiguous()
-        batch_dict['voxel_seg_labels'] = batch_dict['voxel_point_seg_labels'][:, :, 1].median(dim=1)[0]
+        voxel_seg_labels = []
+        indices0 = torch.arange(voxel_features.shape[0]
+                               ).unsqueeze(-1).to(voxel_num_points) # [N, 1]
+        indices1 = torch.arange(voxel_features.shape[1]
+                               ).unsqueeze(0).to(voxel_num_points) # [1, C]
+        indices1 = indices1 % voxel_num_points.unsqueeze(-1) # [1, C] % [N, 1] = [N, C]
+        voxel_seg_labels = batch_dict['voxel_point_seg_labels'][:, :, 1] # [N, C]
+        voxel_seg_labels_median = voxel_seg_labels[(indices0, indices1)].median(-1)[0]
 
+        batch_dict['voxel_seg_labels'] = voxel_seg_labels_median
         return batch_dict
