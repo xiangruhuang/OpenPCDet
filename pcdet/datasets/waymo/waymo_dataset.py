5b8db4656a8771af981be753e8ef175564555dfe
xiangruhuang <xiangruhuang816@gmail.com>
2022-05-29 17:35:55 -0400

    compatibility

--- pcdet/datasets/waymo/waymo_dataset.py
+++ pcdet/datasets/waymo/waymo_dataset.py
@@ -37,10 +37,40 @@ SIZE = {
     'waymo_seg_with_r2_top_toy_training.box_label_attr': 237,
     'waymo_seg_with_r2_top_toy_training.top_lidar_origin': 237,
     'waymo_seg_with_r2_top_toy_training.db_point_feat_label': 28637,
+    'waymo_seg_with_r2_top_validation.point': 5996,
+    'waymo_seg_with_r2_top_validation.label': 5996,
+    'waymo_seg_with_r2_top_validation.instance': 5996,
+    'waymo_seg_with_r2_top_validation.box_label_attr': 5996,
     'waymo_seg_with_r2_top_toy_validation.point': 60,
     'waymo_seg_with_r2_top_toy_validation.label': 60,
     'waymo_seg_with_r2_top_toy_validation.instance': 60,
     'waymo_seg_with_r2_top_toy_validation.box_label_attr': 60,
+}
+
+ALIAS = {
+    'waymo_seg_with_r2_top_training.point': 'waymo.point',
+    'waymo_seg_with_r2_top_training.label': 'waymo.label',
+    'waymo_seg_with_r2_top_training.instance': 'waymo.instance',
+    'waymo_seg_with_r2_top_training.box_label_attr': 'waymo.bbox',
+    'waymo_seg_with_r2_top_training.top_lidar_origin': 'waymo.top_lidar_origin',
+    'waymo_seg_with_r2_top_training.db_point_feat_label': 'waymo.db_point_feat_label',
+    'waymo_seg_with_r2_top_validation.point': 'waymo.point',
+    'waymo_seg_with_r2_top_validation.label': 'waymo.label',
+    'waymo_seg_with_r2_top_validation.instance': 'waymo.instance',
+    'waymo_seg_with_r2_top_validation.box_label_attr': 'waymo.bbox',
+}
+
+OFFSET = {
+    'waymo_seg_with_r2_top_training.point': 0,
+    'waymo_seg_with_r2_top_training.label': 0,
+    'waymo_seg_with_r2_top_training.instance': 0,
+    'waymo_seg_with_r2_top_training.box_label_attr': 0,
+    'waymo_seg_with_r2_top_training.top_lidar_origin': 0,
+    'waymo_seg_with_r2_top_training.db_point_feat_label': 0,
+    'waymo_seg_with_r2_top_validation.point': 23691,
+    'waymo_seg_with_r2_top_validation.label': 23691,
+    'waymo_seg_with_r2_top_validation.instance': 23691,
+    'waymo_seg_with_r2_top_validation.box_label_attr': 23691,
 }
 
 class WaymoDataset(DatasetTemplate):
@@ -53,7 +83,8 @@ class WaymoDataset(DatasetTemplate):
         self.data_tag = self.dataset_cfg.PROCESSED_DATA_TAG
         self.split = self.dataset_cfg.DATA_SPLIT[self.mode]
         self.repeat = self.dataset_cfg.get("REPEAT", None)
-        self._index_list = np.arange(self.dataset_cfg.TOTAL_NUM_SAMPLES)
+        sample_offset = self.dataset_cfg.SAMPLE_OFFSET[self.mode]
+        self._index_list = np.arange(sample_offset, sample_offset+self.dataset_cfg.TOTAL_NUM_SAMPLES[self.mode])
         if self.dataset_cfg.SAMPLE_INTERVAL[self.mode] > 1:
             self.logger.info(f"Sample Interval: {self.dataset_cfg.SAMPLE_INTERVAL[self.mode]}")
             self._index_list = self._index_list[::self.dataset_cfg.SAMPLE_INTERVAL[self.mode]]
@@ -105,16 +136,26 @@ class WaymoDataset(DatasetTemplate):
 
     def _allocate_data(self, data_tag, split, data_type, root_path):
         data_name = f'{data_tag}_{split}.{data_type}'
+        if data_name in OFFSET:
+            offset = OFFSET[data_name]
+        else:
+            offset = 0
         num_samples = SIZE[data_name]
-        # allocate data to shm:///
-        path_template = f'{data_tag}_{split}_{{}}.{data_type}'
+        if data_name in ALIAS:
+            data_name = ALIAS[data_name]
+            # allocate data to shm:///
+            path_template = data_name.replace('.', '_{}.')
+        else:
+            path_template = f'{data_tag}_{split}_{{}}.{data_type}'
+
+        max_sample_id = num_samples + offset - 1
         #if len(glob.glob(f'/dev/shm/{data_tag}_{split}_*.{data_type}')) < num_samples:
-        if not os.path.exists(f'/dev/shm/{data_tag}_{split}_{num_samples-1}.{data_type}'):
+        if not os.path.exists('/dev/shm/'+path_template.format(max_sample_id)):
             filename = root_path / data_name
             data_list = joblib.load(filename)
             for idx, data in enumerate(data_list):
-                if not os.path.exists('/dev/shm/'+path_template.format(idx)):
-                    x = SA.create("shm://"+path_template.format(idx), data.shape, dtype=data.dtype)
+                if not os.path.exists('/dev/shm/'+path_template.format(idx+offset)):
+                    x = SA.create("shm://"+path_template.format(idx+offset), data.shape, dtype=data.dtype)
                     x[...] = data[...]
                     x.flags.writeable = False
             del data_list
