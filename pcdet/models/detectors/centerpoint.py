27b3134dea0e9f201056d3e0b6143c976730b9fa
xiangruhuang <xiangruhuang816@gmail.com>
2022-09-01 11:35:17 -0400

    centerpoint

--- pcdet/models/detectors/centerpoint.py
+++ pcdet/models/detectors/centerpoint.py
@@ -22,6 +22,9 @@ class CenterPoint(Detector3DTemplate):
             return ret_dict, tb_dict, disp_dict
         else:
             pred_dicts, recall_dicts = self.post_processing(batch_dict)
+            for pred_dict, frame_id in zip(pred_dicts, batch_dict['frame_id'].reshape(-1)):
+                pred_dict['scene_wise']['frame_id'] = frame_id
+            pred_dicts
             return pred_dicts, recall_dicts
 
     def get_training_loss(self):
@@ -39,10 +42,25 @@ class CenterPoint(Detector3DTemplate):
     def post_processing(self, batch_dict):
         post_process_cfg = self.model_cfg.POST_PROCESSING
         batch_size = batch_dict['batch_size']
-        final_pred_dict = batch_dict['final_box_dicts']
+        final_pred_dicts = batch_dict['final_box_dicts']
+        pred_dicts = []
+        for i, final_pred_dict in enumerate(final_pred_dicts):
+            pred_dict = dict(
+                point_wise=dict(),
+                object_wise=dict(
+                    pred_box_attr=final_pred_dict['pred_boxes'],
+                    pred_box_scores=final_pred_dict['pred_scores'],
+                    pred_box_cls_label=final_pred_dict['pred_labels'],
+                ),
+                scene_wise=dict(
+                    frame_id=batch_dict['frame_id'].reshape(-1)[i],
+                )
+            )
+            pred_dicts.append(pred_dict)
+                
         recall_dict = {}
         for index in range(batch_size):
-            pred_boxes = final_pred_dict[index]['pred_boxes']
+            pred_boxes = final_pred_dicts[index]['pred_boxes']
 
             recall_dict = self.generate_recall_record(
                 box_preds=pred_boxes,
@@ -50,4 +68,4 @@ class CenterPoint(Detector3DTemplate):
                 thresh_list=post_process_cfg.RECALL_THRESH_LIST
             )
 
-        return final_pred_dict, recall_dict
+        return pred_dicts, recall_dict
