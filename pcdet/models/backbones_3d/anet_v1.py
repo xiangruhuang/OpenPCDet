0ed3bac3dfafc84ad958bb026da7d839310deb37
xiangruhuang <xiangruhuang816@gmail.com>
2022-10-01 14:34:16 -0400

    anet v1

--- pcdet/models/backbones_3d/anet_v1.py
+++ pcdet/models/backbones_3d/anet_v1.py
@@ -32,91 +32,6 @@ class ANetV1(UNetTemplate):
     def __init__(self, runtime_cfg, model_cfg):
         super(ANetV1, self).__init__(runtime_cfg, model_cfg)
         
-        #cur_channel = self.input_channels
-
-        # EdgeConv Ops
-        #self.point2grid_convs = nn.ModuleList()
-        #self.grid2point_convs = nn.ModuleList()
-        #self.grid2grid_convs = nn.ModuleList()
-        #channel_stack = []
-        #for i in range(self.num_down_modules):
-        #    sc = [int(c*self.scale) for c in self.sa_channels[i]]
-
-        #    # grid to point
-        #    conv = self.build_edge_conv(cur_channel, sc)
-        #    self.grid2point_convs.append(conv)
-
-        #    # point to grid
-        #    conv = build_edge_conv(cur_channel, sc)
-        #    self.point2grid_convs.append(conv)
-
-        #    # grid to grid
-        #    self.build_grid_conv(cur_channel, sc, self.keys[i])
-        #    self.grid2grid_convs.append(grid_convs)
-        #    cur_channel = sc[-1]
-        #    channel_stack.append(cur_channel)
-
-        #self.up_modules = nn.ModuleList()
-        #self.merge_modules = nn.ModuleList()
-        #self.skip_modules = nn.ModuleList()
-        #for i in range(self.num_up_modules):
-        #    fc = [int(c*self.scale) for c in self.fp_channels[i]]
-        #    fc0, fc1, fc2 = fc[0], fc[1], fc[-1]
-        #    key0, key1, key2 = self.keys[-i-1][:3][::-1]
-        #    skip_channel = channel_stack.pop()
-
-        #    self.skip_modules.append(
-        #        nn.ModuleList([
-        #            GridConv(
-        #                self.assigners[0],
-        #                dict(
-        #                    INPUT_CHANNEL=skip_channel,
-        #                    OUTPUT_CHANNEL=fc0,
-        #                    KEY=key0,
-        #                    NORM_CFG=self.norm_cfg,
-        #                    ACTIVATION=self.activation,
-        #                ),
-        #            ),
-        #            *[GridConv(
-        #                self.assigners[0],
-        #                dict(
-        #                    INPUT_CHANNEL=fc0,
-        #                    OUTPUT_CHANNEL=fc0,
-        #                    KEY=key0,
-        #                    RELU=False,
-        #                    NORM_CFG=self.norm_cfg,
-        #                    ACTIVATION=self.activation,
-        #                ),
-        #            ) for _ in range(len(fc)-2)]
-        #            ]
-        #        ))
-
-        #    self.merge_modules.append(
-        #        GridConv(
-        #            self.assigners[0],
-        #            dict(
-        #                INPUT_CHANNEL=fc0*2,
-        #                OUTPUT_CHANNEL=fc1,
-        #                NORM_CFG=self.norm_cfg,
-        #                ACTIVATION=self.activation,
-        #                KEY=key1,
-        #            ),
-        #        ))
-
-        #    self.up_modules.append(
-        #        GridConv(
-        #            self.assigners[0],
-        #            dict(
-        #                INPUT_CHANNEL=fc1,
-        #                OUTPUT_CHANNEL=fc2,
-        #                NORM_CFG=self.norm_cfg,
-        #                ACTIVATION=self.activation,
-        #                KEY=key2,
-        #            ),
-        #        ))
-
-        #    cur_channel = fc2
-
         self.num_point_features = self.up_convs[4].output_channel
 
     def build_grid_conv(self, input_channel, channels, keys):
@@ -151,11 +66,26 @@ class ANetV1(UNetTemplate):
         concat.feat = merge_feat + concat.feat.view(num_ref_points, -1, 2).sum(dim=2)
         return concat, conv_dict
 
+    def grid_conv_down(self, grids, graphs, i, conv_dict):
+        grids[i+2].feat, conv_dict = self.grid2grid_convs[i][0](grids[i+1], grids[i+2], graphs[f'grid{i+1}{i+2}'], conv_dict)
+        grids[i+2].feat, conv_dict = self.grid2grid_convs[i][1](grids[i+2], grids[i+2], graphs[f'grid{i+2}{i+2}'], conv_dict)
+        grids[i+2].feat, conv_dict = self.grid2grid_convs[i][2](grids[i+2], grids[i+2], graphs[f'grid{i+2}{i+2}'], conv_dict)
+        return grids[i+2].feat, conv_dict
+
+    def conv_down_merge(self, points, grids, graphs, i, conv_dict):
+        point2grid_feat = self.point2grid_convs[i](points[i+1], grids[i+2], graphs[f'point2grid{i+2}'])
+        grid2grid_feat, conv_dict = self.grid_conv_down(grids, graphs, i, conv_dict)
+        grids[i+2].feat = torch.cat([grid2grid_feat, point2grid_feat], dim=-1)
+        grids[i+2].feat, conv_dict = self.down_merge_convs[i](grids[i+2], grids[i+2], graphs[f'grid{i+2}{i+2}'], conv_dict)
+        return grids[i+2].feat, conv_dict
+
     def forward(self, batch_dict):
         points0 = EasyDict(dict())
         for attr in self.input_attributes:
             points0[attr] = batch_dict[f'{self.input_key}_{attr}']
         points0.bcenter = points0.bxyz
+        points = {0: points0}
+
 
         grids1 = EasyDict(dict())
         for attr in self.input_attributes:
@@ -163,100 +93,114 @@ class ANetV1(UNetTemplate):
         grids1.bcenter = batch_dict['voxel_bcenter']
         grids1.bcoords = batch_dict['voxel_bcoords']
         grids1.bxyz = grids1.bcenter
+        grids = {1: grids1}
 
         runtime_dict = EasyDict(dict())
 
         # grid sampling
-        grids2 = self.grid_samplers[0](grids1, runtime_dict)
-        grids3 = self.grid_samplers[1](grids2, runtime_dict)
-        grids4 = self.grid_samplers[2](grids3, runtime_dict)
-        grids5 = self.grid_samplers[3](grids4, runtime_dict)
-        grids6 = self.grid_samplers[4](grids5, runtime_dict)
-        grids2.bxyz = grids2.bcenter
-        grids3.bxyz = grids3.bcenter
-        grids4.bxyz = grids4.bcenter
-        grids5.bxyz = grids5.bcenter
-        grids6.bxyz = grids6.bcenter
+        for i in range(2, 7):
+            grids[i] = self.grid_samplers[i-2](grids[i-1], runtime_dict)
+            grids[i].bxyz = grids[i].bcenter
+        #grids2 = self.grid_samplers[0](grids1, runtime_dict)
+        #grids3 = self.grid_samplers[1](grids2, runtime_dict)
+        #grids4 = self.grid_samplers[2](grids3, runtime_dict)
+        #grids5 = self.grid_samplers[3](grids4, runtime_dict)
+        #grids6 = self.grid_samplers[4](grids5, runtime_dict)
+        #grids2.bxyz = grids2.bcenter
+        #grids3.bxyz = grids3.bcenter
+        #grids4.bxyz = grids4.bcenter
+        #grids5.bxyz = grids5.bcenter
+        #grids6.bxyz = grids6.bcenter
 
         # point sampling
-        points1 = self.point_samplers[0](points0, runtime_dict)
-        points2 = self.point_samplers[1](points1, runtime_dict)
-        points3 = self.point_samplers[2](points2, runtime_dict)
-        points4 = self.point_samplers[3](points3, runtime_dict)
-        points5 = self.point_samplers[4](points4, runtime_dict)
+        for i in range(1, 6):
+            points[i] = self.point_samplers[i-1](points[i-1], runtime_dict)
+        #points1 = self.point_samplers[0](points0, runtime_dict)
+        #points2 = self.point_samplers[1](points1, runtime_dict)
+        #points3 = self.point_samplers[2](points2, runtime_dict)
+        #points4 = self.point_samplers[3](points3, runtime_dict)
+        #points5 = self.point_samplers[4](points4, runtime_dict)
 
         # graph building
         graphs = {}
-        graphs['point2grid2'] = self.point2grid_graphs[0](points1, grids2)
-        graphs['point2grid3'] = self.point2grid_graphs[1](points2, grids3)
-        graphs['point2grid4'] = self.point2grid_graphs[2](points3, grids4)
-        graphs['point2grid5'] = self.point2grid_graphs[3](points4, grids5)
-        graphs['point2grid6'] = self.point2grid_graphs[4](points5, grids6)
+        for i in range(2, 7):
+            graphs[f'point2grid{i}'] = self.point2grid_graphs[i-2](points[i-1], grids[i])
+        #graphs['point2grid2'] = self.point2grid_graphs[0](points1, grids2)
+        #graphs['point2grid3'] = self.point2grid_graphs[1](points2, grids3)
+        #graphs['point2grid4'] = self.point2grid_graphs[2](points3, grids4)
+        #graphs['point2grid5'] = self.point2grid_graphs[3](points4, grids5)
+        #graphs['point2grid6'] = self.point2grid_graphs[4](points5, grids6)
 
-        graphs['grid2point1'] = self.grid2point_graphs[0](grids1, points1)
-        graphs['grid2point2'] = self.grid2point_graphs[1](grids2, points2)
-        graphs['grid2point3'] = self.grid2point_graphs[2](grids3, points3)
-        graphs['grid2point4'] = self.grid2point_graphs[3](grids4, points4)
-        graphs['grid2point5'] = self.grid2point_graphs[4](grids5, points5)
+        for i in range(1, 6):
+            graphs[f'grid2point{i}'] = self.grid2point_graphs[i-1](grids[i], points[i])
+        #graphs['grid2point1'] = self.grid2point_graphs[0](grids1, points1)
+        #graphs['grid2point2'] = self.grid2point_graphs[1](grids2, points2)
+        #graphs['grid2point3'] = self.grid2point_graphs[2](grids3, points3)
+        #graphs['grid2point4'] = self.grid2point_graphs[3](grids4, points4)
+        #graphs['grid2point5'] = self.grid2point_graphs[4](grids5, points5)
 
-        graphs['grid11'] = self.grid2grid_graphs[0](grids1, grids1)
-        graphs['grid12'] = self.grid2grid_graphs[0](grids1, grids2)
-        graphs['grid22'] = self.grid2grid_graphs[0](grids2, grids2)
-        graphs['grid23'] = self.grid2grid_graphs[0](grids2, grids3)
-        graphs['grid33'] = self.grid2grid_graphs[1](grids3, grids3)
-        graphs['grid34'] = self.grid2grid_graphs[1](grids3, grids4)
-        graphs['grid44'] = self.grid2grid_graphs[2](grids4, grids4)
-        graphs['grid45'] = self.grid2grid_graphs[2](grids4, grids5)
-        graphs['grid55'] = self.grid2grid_graphs[3](grids5, grids5)
-        graphs['grid56'] = self.grid2grid_graphs[3](grids5, grids6)
-        graphs['grid66'] = self.grid2grid_graphs[4](grids6, grids6)
+        graphs['grid11'] = self.grid2grid_graphs[0](grids[1], grids[1])
+        graphs['grid12'] = self.grid2grid_graphs[0](grids[1], grids[2])
+        graphs['grid22'] = self.grid2grid_graphs[0](grids[2], grids[2])
+        graphs['grid23'] = self.grid2grid_graphs[0](grids[2], grids[3])
+        graphs['grid33'] = self.grid2grid_graphs[1](grids[3], grids[3])
+        graphs['grid34'] = self.grid2grid_graphs[1](grids[3], grids[4])
+        graphs['grid44'] = self.grid2grid_graphs[2](grids[4], grids[4])
+        graphs['grid45'] = self.grid2grid_graphs[2](grids[4], grids[5])
+        graphs['grid55'] = self.grid2grid_graphs[3](grids[5], grids[5])
+        graphs['grid56'] = self.grid2grid_graphs[3](grids[5], grids[6])
+        graphs['grid66'] = self.grid2grid_graphs[4](grids[6], grids[6])
 
         # computation
-        points1.feat = self.grid2point_convs[0](grids1, points1, graphs['grid2point1'])
+        points[1].feat = self.grid2point_convs[0](grids[1], points[1], graphs['grid2point1']) # [P1, 16]
         
-        grids2.feat = self.point2grid_convs[0](points1, grids2,  graphs['point2grid2'])
-        points2.feat = self.grid2point_convs[1](grids2, points2, graphs['grid2point2'])
+        grids[2].feat, runtime_dict = self.conv_down_merge(points, grids, graphs, 0, runtime_dict)
+        points[2].feat = self.grid2point_convs[1](grids[2], points[2], graphs['grid2point2'])
         
-        grids3.feat = self.point2grid_convs[1](points2, grids3,  graphs['point2grid3'])
-        points3.feat = self.grid2point_convs[2](grids3, points3, graphs['grid2point3'])
+        #grids[3].feat = self.point2grid_convs[1](points[2], grids[3],  graphs['point2grid3'])
+        grids[3].feat, runtime_dict = self.conv_down_merge(points, grids, graphs, 1, runtime_dict)
+        points[3].feat = self.grid2point_convs[2](grids[3], points[3], graphs['grid2point3'])
 
-        grids4.feat = self.point2grid_convs[2](points3, grids4,  graphs['point2grid4'])
-        points4.feat = self.grid2point_convs[3](grids4, points4, graphs['grid2point4'])
+        #grids[4].feat = self.point2grid_convs[2](points[3], grids[4],  graphs['point2grid4'])
+        grids[4].feat, runtime_dict = self.conv_down_merge(points, grids, graphs, 2, runtime_dict)
+        points[4].feat = self.grid2point_convs[3](grids[4], points[4], graphs['grid2point4'])
 
-        grids5.feat = self.point2grid_convs[3](points4, grids5,  graphs['point2grid5'])
-        points5.feat = self.grid2point_convs[4](grids5, points5, graphs['grid2point5'])
+        #grids[5].feat = self.point2grid_convs[3](points[4], grids[5],  graphs['point2grid5'])
+        grids[5].feat, runtime_dict = self.conv_down_merge(points, grids, graphs, 3, runtime_dict)
+        points[5].feat = self.grid2point_convs[4](grids[5], points[5], graphs['grid2point5'])
         
-        grids6.feat = self.point2grid_convs[4](points5, grids6,  graphs['point2grid6'])
+        grids[6].feat, runtime_dict = self.conv_down_merge(points, grids, graphs, 4, runtime_dict)
+        #grids[6].feat = self.point2grid_convs[4](points[5], grids[6],  graphs['point2grid6'])
 
         reversed_graphs = reverse_graphs(graphs)
-        grids6.up_feat = grids6.feat
+        grids[6].up_feat = grids[6].feat
         
-        merge6, runtime_dict = self.conv_up(grids6, 0, reversed_graphs['grid66'], runtime_dict)
-        grids5.up_feat, runtime_dict = self.up_convs[0](merge6, grids5, reversed_graphs['grid56'], runtime_dict)
+        merge6, runtime_dict = self.conv_up(grids[6], 0, reversed_graphs['grid66'], runtime_dict)
+        grids[5].up_feat, runtime_dict = self.up_convs[0](merge6, grids[5], reversed_graphs['grid56'], runtime_dict)
 
-        merge5, runtime_dict = self.conv_up(grids5, 1, reversed_graphs['grid55'], runtime_dict)
-        grids4.up_feat, runtime_dict = self.up_convs[1](merge5, grids4, reversed_graphs['grid45'], runtime_dict)
+        merge5, runtime_dict = self.conv_up(grids[5], 1, reversed_graphs['grid55'], runtime_dict)
+        grids[4].up_feat, runtime_dict = self.up_convs[1](merge5, grids[4], reversed_graphs['grid45'], runtime_dict)
         
-        merge4, runtime_dict = self.conv_up(grids4, 2, reversed_graphs['grid44'], runtime_dict)
-        grids3.up_feat, runtime_dict = self.up_convs[2](merge4, grids3, reversed_graphs['grid34'], runtime_dict)
+        merge4, runtime_dict = self.conv_up(grids[4], 2, reversed_graphs['grid44'], runtime_dict)
+        grids[3].up_feat, runtime_dict = self.up_convs[2](merge4, grids[3], reversed_graphs['grid34'], runtime_dict)
 
-        merge3, runtime_dict = self.conv_up(grids3, 3, reversed_graphs['grid33'], runtime_dict)
-        grids2.up_feat, runtime_dict = self.up_convs[3](merge3, grids2, reversed_graphs['grid23'], runtime_dict)
+        merge3, runtime_dict = self.conv_up(grids[3], 3, reversed_graphs['grid33'], runtime_dict)
+        grids[2].up_feat, runtime_dict = self.up_convs[3](merge3, grids[2], reversed_graphs['grid23'], runtime_dict)
         
-        merge2, runtime_dict = self.conv_up(grids2, 4, reversed_graphs['grid22'], runtime_dict)
-        grids1.up_feat, runtime_dict = self.up_convs[4](merge2, grids1, reversed_graphs['grid12'], runtime_dict)
+        merge2, runtime_dict = self.conv_up(grids[2], 4, reversed_graphs['grid22'], runtime_dict)
+        grids[1].up_feat, runtime_dict = self.up_convs[4](merge2, grids[1], reversed_graphs['grid12'], runtime_dict)
 
         if self.output_key is not None:
-            batch_dict[f'{self.output_key}_feat'] = grids1.up_feat
-            batch_dict[f'{self.output_key}_bxyz'] = grids1.bcenter
+            batch_dict[f'{self.output_key}_feat'] = grids[1].up_feat
+            batch_dict[f'{self.output_key}_bxyz'] = grids[1].bcenter
 
         # update batch_dict
-        for i, grids in enumerate([grids1, grids2, grids3, grids4, grids5, grids6]):
-            key = f'grids{i+1}'
+        for i, grids in grids.items():
+            key = f'grids{i}'
             for attr in grids.keys():
                 batch_dict[f'{key}_{attr}'] = grids[attr]
         
-        for i, points in enumerate([points0, points1, points2, points3, points4, points5]):
+        for i, points in points.items():
             key = f'points{i}'
             for attr in points.keys():
                 batch_dict[f'{key}_{attr}'] = points[attr]
