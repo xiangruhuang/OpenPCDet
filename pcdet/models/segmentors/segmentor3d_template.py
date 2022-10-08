522e2aed540beb4f3ab937d5e944e6b2ff8e8d50
xiangruhuang <xiangruhuang816@gmail.com>
2022-10-08 15:53:49 -0400

    ema

--- pcdet/models/segmentors/segmentor3d_template.py
+++ pcdet/models/segmentors/segmentor3d_template.py
@@ -203,6 +203,37 @@ class Segmentor3DTemplate(nn.Module):
             state_dict.update(update_model_state)
             self.load_state_dict(state_dict)
         return state_dict, update_model_state
+    
+    def _load_and_ema_state_dict(self, model_state_disk, momentum, *, strict=True):
+        state_dict = self.state_dict()  # local cache of state_dict
+
+        spconv_keys = find_all_spconv_keys(self)
+
+        update_model_state = {}
+        for key, val in model_state_disk.items():
+            if key in spconv_keys and key in state_dict and state_dict[key].shape != val.shape:
+                # with different spconv versions, we need to adapt weight shapes for spconv blocks
+                # adapt spconv weights from version 1.x to version 2.x if you used weights from spconv 1.x
+
+                val_native = val.transpose(-1, -2)  # (k1, k2, k3, c_in, c_out) to (k1, k2, k3, c_out, c_in)
+                if val_native.shape == state_dict[key].shape:
+                    val = val_native.contiguous()
+                else:
+                    assert val.shape.__len__() == 5, 'currently only spconv 3D is supported'
+                    val_implicit = val.permute(4, 0, 1, 2, 3)  # (k1, k2, k3, c_in, c_out) to (c_out, k1, k2, k3, c_in)
+                    if val_implicit.shape == state_dict[key].shape:
+                        val = val_implicit.contiguous()
+
+            if key in state_dict and state_dict[key].shape == val.shape:
+                update_model_state[key] = val * (1-momentum) + state_dict[key] * momentum
+                # logger.info('Update weight %s: %s' % (key, str(val.shape)))
+
+        if strict:
+            self.load_state_dict(update_model_state)
+        else:
+            state_dict.update(update_model_state)
+            self.load_state_dict(state_dict)
+        return state_dict, update_model_state
 
     def load_params_from_file(self, filename, logger, to_cpu=False):
         if not os.path.isfile(filename):
@@ -226,6 +257,33 @@ class Segmentor3DTemplate(nn.Module):
                 logger.info('Updated weight %s in state dict from file: %s' % (key, str(state_dict[key].shape)))
 
         logger.info('==> Done (loaded %d/%d)' % (len(update_model_state), len(state_dict)))
+    
+    def load_ema_params_from_files(self, filenames, logger, momentum, to_cpu=False):
+        for i, filename in enumerate(filenames):
+            if not os.path.isfile(filename):
+                raise FileNotFoundError
+
+            logger.info('==> Loading parameters from checkpoint %s to %s' % (filename, 'CPU' if to_cpu else 'GPU'))
+            loc_type = torch.device('cpu') if to_cpu else None
+            checkpoint = torch.load(filename, map_location=loc_type)
+            model_state_disk = checkpoint['model_state']
+
+            version = checkpoint.get("version", None)
+            if version is not None:
+                logger.info('==> Checkpoint trained from version: %s' % version)
+
+            if i == 0:
+                state_dict, update_model_state = self._load_state_dict(model_state_disk, strict=False)
+            else:
+                state_dict, update_model_state = self._load_and_ema_state_dict(model_state_disk, momentum, strict=False)
+
+            for key in state_dict:
+                if key not in update_model_state:
+                    logger.info('Not updated weight %s in state dict: %s, no key matching' % (key, str(state_dict[key].shape)))
+                else:
+                    logger.info('Updated weight %s in state dict from file: %s' % (key, str(state_dict[key].shape)))
+
+            logger.info('==> Done (loaded %d/%d)' % (len(update_model_state), len(state_dict)))
 
     def load_params_with_optimizer(self, filename, to_cpu=False, optimizer=None, logger=None):
         if not os.path.isfile(filename):
